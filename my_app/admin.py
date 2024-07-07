41b2e22f4d9d4a76dacd13d5625cb934b679ea8e
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-07-07 17:27:15 +0300

    Работа с файлами. Картинка должна быть добавлена обязательно

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 from .models import Author, Book, Genre, Language, Publisher, Status, BookInstance
 
@@ -7,14 +9,26 @@ class BooksInstanceInline(admin.TabularInline):
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name')
+    list_display = ('first_name', 'last_name', 'show_photo')
     fields = ['last_name', 'first_name', ('date_of_birth', 'photo')]
+    readonly_fields = ["show_photo"]
+    def show_photo(self, obj):
+        return format_html(f'<img src="{obj.photo.url}" style="max-height: 100px;">')
+        # можно и с использованием функции mark_safe
+        # return mark_safe(f'<img src="{obj.photo.url}" style="max-height: 100px;">')
+
+    show_photo.short_description = 'Фото'
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'genre', 'language', 'display_author')
+    list_display = ('title', 'genre', 'language', 'display_author', 'show_photo')
     list_filter = ('genre', 'author')
     inlines = [BooksInstanceInline]
+    readonly_fields = ["show_photo"]
+    def show_photo(self, obj):
+        # return "555"
+        return format_html(f'<img src="{obj.photo.url}" style="max-height: 100px;">')
+    show_photo.short_description = 'Обложка'
 
 
 @admin.register(Genre)
--- my_project/urls.py
+++ my_project/urls.py
@@ -3,7 +3,15 @@ from django.urls import path
 
 from my_app import views
 
+# добавлено для показа картинок
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
