2bc0fac611b1e6d4731b65a2e9622006b7f1ea56
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-07-07 20:41:33 +0300

    Базовый шаблон

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Главная страница сайта  Мир книг!")
+
+    # Словарь для передачи данных в шаблон
+    text_head = 'Это заголовок главной страницы сайта'
+    text_body = 'Это содержимое главной страницы сайта'
+    context = {'text_head': text_head, 'text_body': text_body}
+
+    # передача словаря context с данными в шаблон
+    return render(request, 'my_app/index.html', context)
