9dc33a4465ded498e01c6833dac433d72bf20914
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-07-07 21:31:57 +0300

    Базовые шаблоны

--- my_app/urls.py
+++ my_app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from my_app import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index_page_1, name='name_index_page_1'),
+    path('/b2', views.page_2, name='name_page_2'),
+    path('/b3', views.page_3, name='name_page_3'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
 
 
-def index(request):
+def index_page_1(request):
 
     # Словарь для передачи данных в шаблон
-    text_head = 'Это заголовок главной страницы сайта'
-    text_body = 'Это содержимое главной страницы сайта'
-    context = {'text_head': text_head, 'text_body': text_body}
+    index_page_1_head_zagolovok_page_1 = 'index_page_1_head_zagolovok_page_1'
+    index_page_1_body_telo_page_1 = 'index_page_1_body_telo_page_1'
+    context = {'index_page_1_head_zagolovok_page_1': index_page_1_head_zagolovok_page_1,
+               'index_page_1_body_telo_page_1': index_page_1_body_telo_page_1}
 
     # передача словаря context с данными в шаблон
-    return render(request, 'my_app/index.html', context)
+    return render(request, 'my_app/index_page_1.html', context)
+
+def page_2(request):
+
+    # Словарь для передачи данных в шаблон
+    page_2_head_zagolovok_page_1 = 'page_2_head_zagolovok_page_1'
+    page_2_body_telo_page_1 = 'page_2_body_telo_page_1'
+    context = {'page_2_head_zagolovok_page_1': page_2_head_zagolovok_page_1,
+               'page_2_body_telo_page_1': page_2_body_telo_page_1}
+
+    # передача словаря context с данными в шаблон
+    return render(request, 'my_app/page_2.html', context)
+
+def page_3(request):
+
+    # Словарь для передачи данных в шаблон
+    page_3_head_zagolovok_page_1 = 'page_3_head_zagolovok_page_1'
+    page_3_body_telo_page_1 = 'page_3_body_telo_page_1'
+    context = {'page_3_head_zagolovok_page_1': page_3_head_zagolovok_page_1,
+               'page_3_body_telo_page_1': page_3_body_telo_page_1}
+
+    # передача словаря context с данными в шаблон
+    return render(request, 'my_app/page_3.html', context)
